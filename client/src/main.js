b6191e2daa8424bd75852204f0682e4dede63cb1
SebastianCarai <sebastiancarai7@gmail.com>
2024-06-13 09:40:53 +0200

    improved security

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,4 @@
 import './assets/main.css'
-// let googleApiScript = document.createElement('script');  
-// googleApiScript.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places&v=weekly`);
-// document.head.appendChild(googleApiScript);
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
@@ -17,7 +13,10 @@ import PostFeed from './components/core_components/PostFeed.vue';
 import MobileBottomNav from './components/sections/mobile/BottomNav.vue';
 import BurgerMenu from './components/sections/mobile/BurgerMenu.vue';
 
-const app = createApp(App)
+const app = createApp(App);
+
+let googleApiScript = document.getElementById('google-maps');  
+googleApiScript.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_API_KEY}&libraries=places&v=weekly`);
 
 app.component('DesktopLeftNav', DesktopLeftNav)
 app.component('DesktopRightNav', DesktopRightNav)
